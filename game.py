d325d7fcf5829411b4f39bea3f043e01fd373a2b
JLAcosta1 <mathassistant3.14@gmail.com>
2022-02-23 23:59:23 -0700

    Added docstrings for functions.

--- game.py
+++ game.py
@@ -14,61 +14,62 @@ def chess():
           if i == 0 or i == 7:
             color = 'Black' if i == 0 else 'White'
             if j == 0:
-              #Sets Rooks
+              # Sets Rooks
 
               current = self.spotList[i][j]
               current.setPiece(Rook(color))
 
             elif j == 1:
-
+              # Sets Knight
               current = self.spotList[i][j]
               current.setPiece(Knight(color))
               
 
             elif j == 2:
-
+              # Sets Bishop
               current = self.spotList[i][j]
               current.setPiece(Bishop(color))
 
 
             elif j == 3:
+              # Sets Queen
               current = self.spotList[i][j]
               current.setPiece(Queen(color))
 
 
             elif j == 4:
+              # Sets King
               current = self.spotList[i][j]
               current.setPiece(King(color))
 
             elif j == 5:
-              #Sets Rooks
-
+              # Sets Bishop
               current = self.spotList[i][j]
               current.setPiece(Bishop(color))
 
             elif j == 6:
-              #Sets Rooks
-
+              # Sets Knights
               current = self.spotList[i][j]
               current.setPiece(Knight(color))
 
             elif j == 7:
-              #Sets Rooks
-
+              # Sets Rook
               current = self.spotList[i][j]
               current.setPiece(Rook(color))
 
           elif i == 1:
+            # Sets Pawns for File 2
             current = self.spotList[i][j]
             current.setPiece(Pawn('Black'))
 
           elif i == 6:
+            # Sets Pawns for File 3
             current = self.spotList[i][j]
             current.setPiece(Pawn('White'))
 
 
     def getBoardById(self): 
-      '''Returns an 8x8 Grid showing tite (Spot) ID'''
+      '''Returns an 8x8 grid showing tite (Spot) ID'''
       for i in range(len(self.spotList)):
 
         for j in range(len(self.spotList[i])):
@@ -78,6 +79,7 @@ def chess():
         print()
 
     def getBoardBySymbol(self):
+      '''Returns an 8x8 grid showing Pieces on Board by their Symbol'''
       for i in range(len(self.spotList)):
 
         for j in range(len(self.spotList[i])):
@@ -87,6 +89,7 @@ def chess():
         print()
 
     def getBoardByColor(self):
+      '''Returns an 8x8 grid showing Pieces on Board by their Color'''
       for i in range(len(self.spotList)):
 
         for j in range(len(self.spotList[i])):
@@ -105,17 +108,21 @@ def chess():
       self.piece = None
 
     def setPiece(self, piece):
+      '''Assign Piece Object to Spot'''
       self.piece = piece
 
     def getId(self):
+      '''Return id value of Spot'''
       return self.id
 
     def getSymbol(self):
+      '''Return Symbol attribute of Piece assigned to Spot'''
       if self.piece == None:
         return '-'
       return self.piece.getSymbol()
 
     def getColor(self):
+      '''Return Color attribute of Piece assigned to Spot'''
       if self.piece == None:
         return '-'
       return self.piece.getColor()
@@ -129,9 +136,11 @@ def chess():
       self.color = color
 
     def getSymbol(self):
+      '''Return Symbol value of Piece'''
       return self.symbol
 
     def getColor(self):
+      '''Return first character of Color value of Piece'''
       return self.color[0]
 
 
